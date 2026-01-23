69b4664b87516379a34333a5c18a11b1825857af
Noor-Dev1417 <noor.empliz@gmail.com>
2026-01-23 19:55:29 +0530

    Basics Of JS

---
+++ firstb.js
@@ -0,0 +1,25 @@
+console.log("Hi Noor");
+console.log("This Is My First JS Code")
+
+var X = 50
+let Y = 70
+
+console.log(X - Y);
+
+var str = "str is a string"
+console.log(str);
+
+let bool = false
+console.log(bool);
+
+var n = null
+console.log(n);
+
+let u = undefined
+console.log(u);
+
+var num = 12345
+console.log(num);
+
+let bigint = 918008838364
+console.log(bigint);
